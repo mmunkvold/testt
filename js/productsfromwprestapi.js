c9abcb0c553cc5ceaa6e1a71a4a61d929ba2e2da
mmunkvold <mmunkvold@yahoo.no>
2021-08-21 22:56:14 +0200

    Updated

--- js/productsfromwprestapi.js
+++ js/productsfromwprestapi.js
@@ -9,8 +9,8 @@ async function getProducts(url) {
   products.forEach(function (product) {
     productContainer.innerHTML += `<div class="product">
     <a href="productdetails.html?id=${product.id}" ><p class="new-label">${product.tags[0].name}</p>
-    <img class="product-image" src="${product.images[0].src}"/><h3>${product.name}</h3>
-    <div class="product-price">Price: ${product.prices.regular_price},-</a></div>
+    <img class="product-image" src="${product.images[0].src}"/><h3>${product.name}</h3></a>
+    <div class="product-price">Price: ${product.prices.regular_price},-</div>
     <div class="center"><button id="newbutton" class="add-to-cart-btn" data-product="${product.id}">Add to cart</button><i class="fas fa-heart"></i></div></div>`;
   });
 }
